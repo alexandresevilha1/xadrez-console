33203020d30ad5c2e7dfc86bc921e8475d87e3b7
Alexandre Sevilha <alexandre.sevilha1@gmail.com>
2023-07-15 09:41:46 -0300

    Criado a classe posicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -2,19 +2,8 @@
 using xadrez;
 using xadrez_console;
 
-Tabuleiro tab = new Tabuleiro(8, 8);
-
-try 
-{ 
-tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-Tela.ImprimirTabuleiro(tab);
-}
-catch (TabuleiroException e)
-{
-    Console.WriteLine(e.Message);
-}
+PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+Console.WriteLine(pos);
+Console.WriteLine(pos.toPosicao());
 
 Console.ReadLine();

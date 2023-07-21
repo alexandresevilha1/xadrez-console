7e52554ab32ef8a4bc08df6923a40d43e492d6e6
Alexandre Sevilha <alexandre.sevilha1@gmail.com>
2023-07-21 09:24:45 -0300

    Controlando as pecas capturadas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -12,11 +12,7 @@ try
         try
         {
             Console.Clear();
-            Tela.ImprimirTabuleiro(partida.tab);
-            Console.WriteLine();
-
-            Console.WriteLine("Turno: " + partida.turno);
-            Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+            Tela.imprimirPartida(partida);
 
             Console.Write("Origem: ");
             Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
